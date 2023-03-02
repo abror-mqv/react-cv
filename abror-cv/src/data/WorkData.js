003d0cb10c07c156268e6623a3ded35007460a04
Abror <mmtklvabrr@gmail.com>
2023-03-02 17:37:19 +0600

    first commit

---
+++ abror-cv/src/data/WorkData.js
@@ -0,0 +1,55 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"IT-Academy: Ассистент Ментора",
+        description:"Отвечал за процесс обучения, консультировал студентов, настраивал рабочее окружение.",
+        tags:["vscode","linux","html", "ccs", "js", "react"],
+        
+        demo:"https://it-academy.kg/",
+    },
+    {
+        id:2,
+        name:"ZettaLab: Frontend стажер",
+        description:"Получил опыт командной работы, прибавил в хард и софт-скиллах. Работа заключалась в верстке ui-компонентов на react",
+        tags:["react","sass", "styled-components", "MuI", "gitlab"],
+        
+        demo:"https://zettalab.vercel.app/"        
+    },
+    {
+        id:3,
+        name:"ZettaSoft: Frontend разработчик",
+        description:'Опыт разработки коммерческих проектов и выкат продукта на рынок. Зона ответственности расширилась до компонентов с "логикой" и работой с REDUX ',
+        tags:["react","nexjs","sass", "Redux", "styled-components", "gitlab", "MuI", "Framer Motion", ],
+        
+        demo:"https://www.instagram.com/zettasoft.io/", 
+    },
+    {
+        id:4,
+        name:"AGK-Media: Frontend разработчик",
+        description:"Разработка веб-приложений с использованием ui-библиотек, в ерстка, верстка, верстка. Первый опыт удаленной работы",
+        tags:["js","DOM - API","html", "sass" ],
+        
+        demo:"https://agk-media.ru/",       
+    },{
+        id:5,
+        name:"Безработный",
+        description:"В это свободное время решил изучать новые (и старые) технологии - Flutter, Django-Rest-Framework, Kali linux, алгоритмы",
+        tags:["Flutter","DRF","Linux", "Как устроен JavaScript.pdf"],
+        
+        demo:"https://www.kinopoisk.ru/film/42664/",
+        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    },
+    {
+        id:6,
+        name:"Мастер по ремонту телефонов/ноутбуков",
+        description:'В долгосрочной перспективе лучше быть "марафонцем", а не "спринтёром". Решил отвлечься от экрана, вооружившись фен-паяльной станцией',
+        tags:["фен","паяльник","припой", "флюз", "ЛБП", "мультиметр"],
+        
+        demo:"https://www.instagram.com/academy_nefely/"        
+    },
+    
+  
+] 
